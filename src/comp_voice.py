bea07186e43d52b2e1a0d6f0a12ef4b310a3632c
Sarah Gu <guwen888@gmail.com>
2020-04-25 14:32:47 -0400

    spectrogram update

--- src/comp_voice.py
+++ src/comp_voice.py
@@ -2,7 +2,13 @@ import os
 import sys
 
 filepath1 = sys.argv[1]
-#filepath2 = sys.argv[2]
+filepath2 = sys.argv[2]
 #os.system("ffmpeg -i" +  filepath1 + " -vn -acodec pcm_s16le -ar 44100 -ac 2 output.wav")
-os.system("sox output.wav -n spectrogram -o sound-original.png")
+os.system("sox " + filepath1 + " -n spectrogram -o sound-original.png")
+os.system("sox " + filepath2 + " -n spectrogram -o sound-new.png")
+
+os.system("sox -m -v 1 " + filepath1 +" -v -1 " + filepath2 + " sound-difference.wav")
+
+os.system("sox sound-difference.wav -n spectrogram -o sound-difference.png")
+
 
