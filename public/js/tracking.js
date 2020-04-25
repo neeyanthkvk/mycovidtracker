5f6e823b85a3b012f2440975c5f3f561efddb967
Neeyanth Kopparapu <neeyanthkvk@gmail.com>
2020-04-25 18:21:36 -0400

    Added Graphs

--- public/js/tracking.js
+++ public/js/tracking.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("#log-modal-button").click(function() {
     $("#logModal").modal();
   });
+  getData()
 });
 
 
@@ -11,7 +12,6 @@ heartoutput.innerHTML = heartslider.value;
 
 var tempslider = document.getElementById("temperature");
 var tempoutput = document.getElementById("temp");
-var dat = {}
 tempoutput.innerHTML = tempslider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
@@ -56,4 +56,80 @@ function chestButton() {
         dat['chest'] = true;
         document.getElementById("chestButton").style.fontWeight = 'bold';
     }
+}
+
+function getData() {
+    var ajax_params = {
+        'url'     : "/log",
+        'type'    : "GET",
+        'data'    : {
+            'data': true
+        },
+        'success' : dataResponse
+    };
+
+    $.ajax(ajax_params);
+}
+
+function dataResponse(logData) {
+    data = {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: 'Temperature',
+                backgroundColor: window.chartColors.red,
+                borderColor: window.chartColors.red,
+                data: [],
+                fill: false,
+            }, {
+                label: 'Heart Rate',
+                backgroundColor: window.chartColors.green,
+                borderColor: window.chartColors.green,
+                data: [],
+                fill: false,
+            }]
+        },
+        options: {
+            responsive: true,
+            title: {
+                display: true,
+                text: 'Your Log Data'
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Value'
+                    }
+                }]
+            }
+        }
+    }
+    logData.logs.forEach((log) => {
+        console.log("DEBUG: " + JSON.stringify(log))
+        var dat = new Date(log.time)
+        data.data.labels.push(dat.getMonth() + "/" + dat.getDay());
+        data.data.datasets[0].data.push(parseInt(log.body.temperature));
+        data.data.datasets[1].data.push(parseInt(log.body.heartRate));
+    });
+    console.log(data)
+    var ctx = document.getElementById('canvas').getContext('2d');
+	window.myLine = new Chart(ctx, data);
 }
