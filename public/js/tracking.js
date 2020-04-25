20854e5955559a9e8820d91e0660dfa231ae3a18
Neeyanth Kopparapu <neeyanthkvk@gmail.com>
2020-04-25 17:47:43 -0400

    Hotfixes, POST ability for logs

--- public/js/tracking.js
+++ public/js/tracking.js
@@ -1,9 +1,17 @@
-var heartslider = document.getElementById("heartmyRange");
+$(document).ready(function() {
+  $("#log-modal-button").click(function() {
+    $("#logModal").modal();
+  });
+});
+
+
+var heartslider = document.getElementById("heartRate");
 var heartoutput = document.getElementById("heart");
 heartoutput.innerHTML = heartslider.value;
 
-var tempslider = document.getElementById("tempmyRange");
+var tempslider = document.getElementById("temperature");
 var tempoutput = document.getElementById("temp");
+var dat = {}
 tempoutput.innerHTML = tempslider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
@@ -14,7 +22,15 @@ tempslider.oninput = function() {
   tempoutput.innerHTML = tempslider.value;
 }
 function postLog() {
-    dat = {}; //?
+    dat = {
+        'heartRate': heartslider.value,
+        'temperature': tempslider.value,
+        'chest': document.getElementById("chest").checked,
+        'cough': document.getElementById("cough").checked,
+        'breath': document.getElementById("breath").checked,
+        'fatigue': document.getElementById("fatigue").checked,
+        'muscle': document.getElementById("muscle").checked,
+    }
     console.log(dat)
     var ajax_params = {
         'url'     : "/log",
@@ -29,4 +45,15 @@ function postLog() {
 function logResponse(response) {
     alert(response);
     window.location.href = '/log';
+}
+
+function chestButton() {
+    if(dat['chest'] == true) {
+        dat['chest'] = false;
+        document.getElementById("chestButton").style.fontWeight = 'normal';
+    }
+    else {
+        dat['chest'] = true;
+        document.getElementById("chestButton").style.fontWeight = 'bold';
+    }
 }
